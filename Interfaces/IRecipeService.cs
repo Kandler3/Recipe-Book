a72412e226280ae20f76df84a05a6379007467b6
Kandler3 <artchuk@icloud.com>
2025-02-26 18:13:02 +0300

    interfaces added

---
+++ Interfaces/IRecipeService.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace Interfaces;
+
+public interface IRecipeService
+{
+    public IList<Recipe> GetRecipes(IRecipesQuery query);
+    public void AddRecipe(Recipe recipe);
+    public void DeleteRecipe(Recipe recipe);
+    public void AddRecipeImage(Recipe recipe, string imagePath);
+}
